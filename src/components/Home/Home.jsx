4b5f4265e6c99c754b44a6763f3485c90fd0902b
neela <nusratneela27@gmail.com>
2023-04-09 16:25:18 +0600

    tshirt cart added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
-    const tshirt = useLoaderData()
+    const tshirts = useLoaderData()
+    const [cart, setCart] = useState([])
+
+    const handleAddToCart = tshirt => {
+
+    }
 
     return (
-        <div>
-            <h2>this is home {tshirt.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+                {
+                    tshirts.map(tshirt => <Tshirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
